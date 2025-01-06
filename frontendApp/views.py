687771a0d503fecc9fe27f35468d50133a7d5556
kimly07 <kimlyra55@gmail.com>
2025-01-06 08:33:04 +0700

    update home page

--- frontendApp/views.py
+++ frontendApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'frontendApp/index.html')
+    return render(request, 'index.html')
